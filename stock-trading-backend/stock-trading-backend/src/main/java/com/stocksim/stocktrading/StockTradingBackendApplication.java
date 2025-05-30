3cdd0e79d6ad2931103fa86b360fa5be0f7dfcae
harman-04 <harmandeep02315@gmail.com>
2025-05-30 22:34:48 +0530

    intializing the project

---
+++ stock-trading-backend/stock-trading-backend/src/main/java/com/stocksim/stocktrading/StockTradingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stocksim.stocktrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradingBackendApplication.class, args);
+	}
+
+}
---
+++ stock-trading-backend/stock-trading-backend/src/test/java/com/stocksim/stocktrading/StockTradingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksim.stocktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
